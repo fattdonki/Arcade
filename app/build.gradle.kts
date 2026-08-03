6cb12737e75c07e75dce976eec9332dcb4b4543a
Arthur <155854171+fattdonki@users.noreply.github.com>
2026-08-03 14:07:43 +0100

    Add debug build type

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ android {
 
 dependencies {
 	implementation(libs.androidx.compose.animation)
+	implementation(libs.androidx.compose.ui.text)
+	implementation(libs.androidx.core.splashscreen)
 	implementation(libs.androidx.core.ktx)
 	implementation(libs.androidx.lifecycle.runtime.ktx)
 	implementation(libs.androidx.activity.compose)
